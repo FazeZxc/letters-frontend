3907803729f81002ae97abc9d8ca640260c50573
FazeZxc <randomuploader.en@gmail.com>
2024-04-25 18:08:43 +0530

    added login btn

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { authState } from '../store/authState'
 import { LogoutButton } from '../components/LogoutButton'
+import { useNavigate } from 'react-router-dom'
 
 export const HomePage = () => {
     const currentAuthState = useRecoilValue(authState)
+    const navigate = useNavigate()
     console.log(currentAuthState)
-    return <div>{currentAuthState != null ? <LogoutButton /> : null}</div>
+    return (
+        <div>
+            <button
+                onClick={() => navigate('/auth/login')}
+                className="btn btn-secondary"
+            >
+                Log In
+            </button>
+            <div>{currentAuthState != null ? <LogoutButton /> : null}</div>
+        </div>
+    )
 }
